080e7018a3810f1b59e1f18b252cc42f8f3d0896
Vram Kupelian <vramkupelian@gmail.com>
2018-03-07 19:55:41 -0800

    Added to server.js.. should be complete

--- server.js
+++ server.js
@@ -3,3 +3,24 @@ var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
 var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.use("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT,function(){
+    console.log("App now listening at port#: " + PORT);
+});
