bc38e6a670e191244c9b74b744624a68a14f8e6f
Vram Kupelian <30922280+vramkupelian@users.noreply.github.com>
2018-03-11 18:46:01 -0700

    Able to add new burgers into database.

--- config/orm.js
+++ config/orm.js
@@ -1,20 +1,25 @@
 var connection = require("./connection.js");
 
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
 var orm = {
-  selectAll: function(tableInput, cb) {
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableInput], function(err, result) {
+  selectAll: function(cb) {
+    var queryString = "SELECT * FROM burgers";
+    connection.query(queryString, function(err, result) {
       if (err) throw err;
      cb(result);
     });
   },
-  insertOne: function( theColumn, theBurgerName, cb) {
+  insertOne: function(  theColumn, theBurgerName, cb) {
     var queryString = "INSERT INTO burgers (??) VALUE ('??'); ";
     console.log(queryString);
     connection.query(queryString, [ theColumn, theBurgerName], function(err, result) {
       if (err) throw err;
       cb(result);
-      console.log("trying to insert one");
     });
   },
   updateOne: function(columnName, theBurger, cb) {
@@ -23,7 +28,7 @@ var orm = {
 
     connection.query(
       queryString,
-      [columnName, theBurger],
+      [ columnName, theBurger],
       function(err, result) {
         if (err) throw err;
         cb(result);
--- models/burger.js
+++ models/burger.js
@@ -2,19 +2,19 @@ var orm = require("../config/orm.js");
 
 var burger = {
     selectAll: function(cb){
-        orm.selectAll("burgers", function(res){
+        orm.selectAll(function(res){
             cb(res);
         });
     },
 
     insertOne:function(theColumn, theBurgerName,cb) {
-        orm.insertOne("burgers", theColumn,theBurgerName, function(res){
+        orm.insertOne(theColumn,theBurgerName, function(res){
             cb(res);
         });
     },
 
     updateOne: function(columnName, theBurger, cb){
-        orm.updateOne("burgers", columnName, theBurger, function(res){
+        orm.updateOne( columnName, theBurger, function(res){
             cb(res);
         });
     }
