bb4ed6f969399e32a83f0dab8f504b021074cc0a
Vram Kupelian <vramkupelian@gmail.com>
2018-03-11 21:51:55 -0700

    Applying JAWSdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_name",
+    });
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
